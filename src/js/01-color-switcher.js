5335719205d54748a5fe908c1731c8a716bf3eee
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-06-03 16:13:27 +0300

    01-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,25 @@
-
-
-
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector(".startBtn");
+const stopBtn = document.querySelector(".stopBtn");
+let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
+};
+
+function changeColor() {
+  let newColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = newColor;
+};
+
+startBtn.addEventListener("click", () => {
+  startBtn.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    changeColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+});
